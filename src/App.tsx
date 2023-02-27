32e8d237dd4050296b017a832b16c2d731b8f861
Aleksandr-Pod <stdfire@gmail.com>
2023-02-27 16:25:06 +0200

    remove onBoarding state

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import PrivateRout from './hocs/PrivateRoute';
 
 const App = () => {
   const dispatch = useAppDispatch();
-  const { isAuth, onBoarding, isLoading, token } = useAppSelector(state => state.user);
+  const { isAuth, isLoading, token } = useAppSelector(state => state.user);
 
   useEffect(() => {
     console.log("isAuth", isAuth);
