a52a96c0a83f0c53bc0b2d4ac351e0a681dc7aae
Aleksandr-Pod <stdfire@gmail.com>
2023-03-01 18:24:58 +0200

    basename

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/VacMan/">
       <PersistGate loading={null} persistor={persistor}>
         <Provider store={store}>
           <App />
