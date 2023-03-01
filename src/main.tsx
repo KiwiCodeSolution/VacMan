c9dfeb66b56ba5dfdaf4ae9d3571febe77cab815
Aleksandr-Pod <stdfire@gmail.com>
2023-03-01 18:32:26 +0200

    remove basename add ./src

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/VacMan/">
+    <BrowserRouter>
       <PersistGate loading={null} persistor={persistor}>
         <Provider store={store}>
           <App />
