18834f764c8f9788f56a4cbcb735928c8bd3fff9
Aleksandr-Pod <stdfire@gmail.com>
2023-03-01 19:30:00 +0200

    Link

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const token = '1677595510238';
@@ -6,9 +7,10 @@ export default function SignUpPage() {
     <div className="container mx-auto px-4">
       <h1 className="text-2xl">Sign Up Page</h1>
       <h2 className="mt-20">Имитация письма с сылкой подтверждения..</h2>
-      <a className="bg-space-200" href={`https://vacman.netlify.app/confirm_email/${token}`}>
-        Подтвердить email
-      </a>
+      {/* <a className="bg-space-200" href={`https://vacman.netlify.app/confirm_email/${token}`}> */}
+      <Link to={`confirm_email/${token}`}>Подтвердить email</Link>
+      {/* Подтвердить email
+      </a> */}
     </div>
   );
 }
