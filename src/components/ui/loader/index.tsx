f43b97860f933d01fd49f93fb5df5f1880826d88
Aleksandr-Pod <stdfire@gmail.com>
2023-03-03 21:03:09 +0200

    fix

--- src/components/ui/loader/index.tsx
+++ src/components/ui/loader/index.tsx
@@ -4,7 +4,11 @@ import triangle from '../../../assets/icons/triangle.svg';
 export default function Loader() {
   return (
     <div className="w-36 h-36 relative">
-      <img src={circle} className="absolute h-full top-0 left-0 z-0 animate-spin 0.8s linear infinite" alt="spinner circle" />
+      <img
+        src={circle}
+        className="absolute h-full top-0 left-0 z-0 animate-spin 0.8s linear infinite"
+        alt="spinner circle"
+      />
       <img src={triangle} className="absolute h-full top-0 left-0 z-2" alt="logo" />
     </div>
   );
