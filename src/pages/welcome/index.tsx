64c1009d076e33bf404c609b6ba53b939fb3a367
VinnikA <vamposht@gmail.com>
2023-03-04 23:23:41 +0200

    palette added

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -5,10 +5,10 @@ export default function Welcome() {
   return (
     <div className="container mx-auto px-4">
       <h1 className="text-2xl mb-4">Welcom (App Main Page)</h1>
-      <Link className="py-2 px-4 border rounded-lg mr-4 bg-slate-400" to="/signup">
+      <Link className="py-2 px-4 border rounded-lg mr-4 bg-bg-light" to="/signup">
         Sign Up
       </Link>
-      <Link className="py-2 px-4 border rounded-lg bg-slate-400" to="/login">
+      <Link className="py-2 px-4 border rounded-lg bg-bg-light" to="/login">
         Log In
       </Link>
     </div>
