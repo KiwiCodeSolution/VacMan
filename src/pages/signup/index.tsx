d0983ed8e700ddd398fefe3c03cddfea38306862
Alexander Khardin <khporter1984@gmail.com>
2023-03-09 19:07:39 +0300

    implement signup form

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,10 +1,13 @@
 import './styles.css';
 
+import RegisterForm from '../../components/forms/RegisterForm';
+
 const token = '1677595510238';
 export default function SignUpPage() {
   return (
     <div className="container mx-auto px-4">
       <h1 className="text-2xl">Sign Up Page</h1>
+      <RegisterForm />
       <h2 className="mt-20">Имитация письма с сылкой подтверждения..</h2>
       <a className="bg-space-200" href={`http://127.0.0.1:5173/confirmEmail/${token}`}>
         Подтвердить email
