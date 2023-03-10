99cad09165e35ab521cf007945bc2bc49385eabd
Alexander Khardin <khporter1984@gmail.com>
2023-03-10 16:27:42 +0300

    implement login form

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -2,19 +2,18 @@ import { FC, ReactElement } from 'react';
 import { Formik, FormikHelpers, FormikProps } from 'formik';
 import { InferType } from 'yup';
 
-import registerSchema from '../../validationSchemas/registerShema';
+import loginSchema from '../../validationSchemas/loginSchema';
 
 import CustomInput from './CustomInput';
 
-type Values = InferType<typeof registerSchema>;
+type Values = InferType<typeof loginSchema>;
 
 const initialValues: Values = {
   email: '',
   password: '',
-  confirmPassword: '',
 };
 
-const RegisterForm: FC = (): ReactElement => {
+const LoginForm: FC = (): ReactElement => {
   const handelFormSubmit = (values: Values, { resetForm }: FormikHelpers<Values>): void => {
     console.log('Form was submitted.');
     console.log('values: ', values);
@@ -22,18 +21,11 @@ const RegisterForm: FC = (): ReactElement => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handelFormSubmit} validationSchema={registerSchema}>
+    <Formik initialValues={initialValues} onSubmit={handelFormSubmit} validationSchema={loginSchema}>
       {({ handleSubmit }: FormikProps<Values>) => (
         <form onSubmit={handleSubmit}>
           <CustomInput name="email" label="Email" placeholder="Type email" id="email" type="email" />
           <CustomInput name="password" label="Password" placeholder="Type password" id="password" type="password" />
-          <CustomInput
-            name="confirmPassword"
-            label="Confirm Password"
-            placeholder="type password"
-            id="confirmPassword"
-            type="password"
-          />
 
           <button type="submit" style={{ border: '1px solid black', marginTop: '16px' }}>
             done
@@ -44,4 +36,4 @@ const RegisterForm: FC = (): ReactElement => {
   );
 };
 
-export default RegisterForm;
+export default LoginForm;
