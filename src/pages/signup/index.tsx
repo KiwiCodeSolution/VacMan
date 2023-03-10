1bb4c6a776a765d0060c7055681b9152d972f555
Aleksandr-Pod <stdfire@gmail.com>
2023-03-10 14:46:04 +0200

    server url change

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -6,6 +6,9 @@ import getGoogleUserData from '../../utilities/googleUserData';
 import RegisterForm from '../../components/forms/RegisterForm';
 import './styles.css';
 
+// const serverURL = 'http://kiwicode.tech:5000';
+const serverURL = 'http://localhost:3030';
+
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTA4NTI5ZjFiNjk5YjQyYTdiZTY2NyIsImlhdCI6MTY3NjU1MTAwOX0.e-quTDPC3_tAlXj56KhZ173OraK6M18fYZD8LSfd32c';
 export default function SignUpPage() {
@@ -15,7 +18,7 @@ export default function SignUpPage() {
     onSuccess: async resp => {
       const userData = await getGoogleUserData(resp.access_token);
       console.log(userData);
-      const { data } = await axios.post('http://localhost:3030/auth/googleAuth', { userData });
+      const { data } = await axios.post(`${serverURL}/auth/googleAuth`, { userData });
       console.log('data from server:', data);
       dispatch(setUser(data));
       dispatch(setIsAuth(true));
@@ -27,7 +30,7 @@ export default function SignUpPage() {
     <div className="container mx-auto px-4">
       <h1 className="text-2xl">Sign Up Page</h1>
       <RegisterForm />
-      <button className="mt-20 bg-bg-grey" type="button" onClick={() => googleRegister()}>
+      <button className="mt-20 bg-bg-light p-2 rounded-md" type="button" onClick={() => googleRegister()}>
         Google Register
       </button>
       <h2 className="mt-20">Имитация письма с сылкой подтверждения..</h2>
