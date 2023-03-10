789317f8f05ec70caf3a39734bcff1f6dc4eed57
Aleksandr-Pod <stdfire@gmail.com>
2023-03-10 13:58:43 +0200

    googleAuth

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,27 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import { persistor, store } from './redux/store';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import App from './App';
 import './index.css';
 
+let clientId = import.meta.env.VITE_CLIENT_ID;
+if (!clientId) {
+  console.log('clientId is undefined');
+  clientId = '';
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <PersistGate loading={null} persistor={persistor}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </PersistGate>
-    </BrowserRouter>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={clientId}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </PersistGate>
+      </BrowserRouter>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
