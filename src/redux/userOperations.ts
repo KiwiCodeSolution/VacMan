32167d9e1025108ca8d45c7eb5c96764acacedcf
Evgenija-P <e.a.poduzova@gmail.com>
2023-03-12 16:51:06 +0200

    slice + operations

--- src/redux/userOperations.ts
+++ src/redux/userOperations.ts
@@ -1,67 +1,85 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = '';
+axios.defaults.baseURL = 'http://localhost:3001';
 
-const setAuthHeader = (token) => {
-  axios.defaults.headers.common.Authorization = Bearer ${token};
+interface IUser {
+  email: string;
+  token: string;
+  isAuth: boolean;
+  onBoarding: boolean;
+  isLoading: boolean;
+  profile: { [key: string]: string };
+}
+
+const setAuthHeader = (responsToken) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${responsToken}`;
 };
 
-const clearAuthHeader = () => {
-  axios.defaults.headers.common.Authorization = '';
-};
+// const clearAuthHeader = () => {
+//   axios.defaults.headers.common.Authorization = '';
+// };
 
-export const registration = createAsyncThunk('auth/register', async (credentials, thunkAPI) => {
-  try {
-    const response = await axios.post('/auth/register', credentials);
-    setAuthHeader(response.data.token);
-    console.log('Congratulations! You are registered!');
+export const registration = createAsyncThunk<IUser[], undefined, { rejectValue: string }>(
+  'user/registration',
+  async (credentials, { rejectWithValue }) => {
+    console.log(credentials);
+    try {
+      const response = await axios.post('/auth/register', credentials);
+      const responsToken = response.data.token;
+      setAuthHeader(responsToken);
+      console.log('Congratulations! You are registered!');
 
-    return response.data;
-  } catch (error) {
-    console.log('Please, try one more');
-    return thunkAPI.rejectWithValue(error.message);
+      return response.data;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.log(`Sorry, registration error: ${error.message}`);
+      return rejectWithValue(error.message);
+    }
   }
-});
+);
 
-export const logIn = createAsyncThunk('auth/login', async (credentials, thunkAPI) => {
+export const logIn = createAsyncThunk('user/login', async (credentials, thunkAPI) => {
   try {
     const response = await axios.post('/auth/login', credentials);
-    setAuthHeader(response.data.token);
+    // setAuthHeader(response.data.token);
     return response.data;
   } catch (error) {
     console.log('Incorrect email or password. Please, try one more');
-    return thunkAPI.rejectWithValue(error.message);
+    return thunkAPI.rejectWithValue(error);
   }
 });
 
-export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
- try {
-   setAuthHeader();
-   const res = await axios.get('/auth/logout');
-   return res.data;
- } catch (error) {
-   console.log('Please, try one more');
-   return thunkAPI.rejectWithValueerror.message);
+export const logOut = createAsyncThunk('user/logout', async (_, thunkAPI) => {
+  try {
+    setAuthHeader();
+    const res = await axios.get('/auth/logout');
+    return res.data;
+  } catch (error) {
+    console.log('Please, try one more');
+    return thunkAPI.rejectWithValue(error);
+  }
 });
 
-export const currentUser = createAsyncThunk('auth/current', async (_, thunkAPI) => {
-   try {
-   setAuthHeader();
-   const res = await axios.get('/auth/current');
-   return res.data;
- } catch (error) {
-   console.log('Please, try one more');
-   return thunkAPI.rejectWithValue(error.message);
+export const currentUser = createAsyncThunk('user/current', async (_, thunkAPI) => {
+  try {
+    setAuthHeader();
+    const res = await axios.get('/auth/current');
+    return res.data;
+  } catch (error) {
+    console.log('Please, try one more');
+    //    return thunkAPI.rejectWithValue(error.message);
+    return thunkAPI.rejectWithValue(error);
+  }
 });
 
-export const emailVerify = createAsyncThunk('auth/emailVerify', async (_, thunkAPI) => {
-   try {
-   setAuthHeader();
-   const res = await axios.get('/auth/emailVerify');
-   return res.data;
- } catch (error) {
-   console.log('Please, try one more');
-   return thunkAPI.rejectWithValue(error.message);
-}
+export const emailVerify = createAsyncThunk('user/emailVerify', async (_, thunkAPI) => {
+  try {
+    setAuthHeader();
+    const res = await axios.get('/auth/emailVerify');
+    return res.data;
+  } catch (error) {
+    console.log('Please, try one more');
+    return thunkAPI.rejectWithValue(error);
+  }
 });
--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from './store';
+import { registration } from './userOperations';
 
 interface IUser {
   email: string;
@@ -38,6 +39,20 @@ const userSlice = createSlice({
       state.profile = payload.profile;
     },
   },
+  // eslint-disable-next-line prettier/prettier
+  extraReducers: (builder) =>
+    builder
+      // eslint-disable-next-line prettier/prettier
+      .addCase(registration.pending, (state) => state)
+      .addCase(registration.fulfilled, (state, action) => {
+        console.log(action.payload);
+        // state.email = action.payload.email;
+        // state.token = action.payload.token;
+        // state.profile = action.payload.profile;
+      })
+      .addCase(registration.rejected, (state, action: PayloadAction<string>) => {
+        console.log(action.payload);
+      }),
 });
 
 export const selectIsAuth = (state: RootState) => state.user.isAuth; // а нахрена это ? <Sander-Pod>
