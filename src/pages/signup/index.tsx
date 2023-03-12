092845947fc2a5483ecab453b952ba81d51cd11e
Aleksandr-Pod <stdfire@gmail.com>
2023-03-12 20:52:51 +0200

    bed server response after login-register

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -18,9 +18,12 @@ export default function SignUpPage() {
   const googleRegister = useGoogleLogin({
     onSuccess: async resp => {
       const userData = await getGoogleUserData(resp.access_token);
-      console.log(userData);
       const { data } = await axios.post(`${serverURL}/auth/googleAuth`, { userData });
-      console.log('data from server:', data);
+      if (!data) {
+        console.log('Error from server');
+        return;
+      }
+      // console.log('data from server:', data);
       dispatch(setUser(data));
       dispatch(setIsAuth(true));
     },
