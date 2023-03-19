f32337e1bb58a1ee66ebd486005eb7d57331e45b
Aleksandr-Pod <stdfire@gmail.com>
2023-03-19 16:52:52 +0200

    avatar width-height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,7 @@ const Header = () => {
   const { profile } = useAppSelector(state => state.user);
   return (
     <div className="flex items-center">
-      {profile.avatar ? (
-        <img src={profile.avatar} className="w-{56} h-{56} rounded-full" alt="avatar" />
-      ) : (
-        <Icons.Avatar />
-      )}
+      {profile.avatar ? <img src={profile.avatar} className="w-14 h-14 rounded-full" alt="avatar" /> : <Icons.Avatar />}
       <p className="ml-4 w-{178} text-xl text-txt-main">Hello, {profile.name || 'unonymous'}</p>
       <div className="w-{34}">
         <Icons.Bell />
