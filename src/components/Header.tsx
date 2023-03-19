bc90bbb84e65173100b26b7b4990c1e8e2a65355
Aleksandr-Pod <stdfire@gmail.com>
2023-03-19 17:11:19 +0200

    height-width

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ const Header = () => {
   return (
     <div className="flex items-center">
       {profile.avatar ? <img src={profile.avatar} className="w-14 h-14 rounded-full" alt="avatar" /> : <Icons.Avatar />}
-      <p className="ml-4 w-{178} text-xl text-txt-main">Hello, {profile.name || 'unonymous'}</p>
-      <div className="w-{34}">
+      <p className="ml-4 w-[178px] text-xl text-txt-main">Hello, {profile.name || 'unonymous'}</p>
+      <div className="w-[34px] h-[34px]">
         <Icons.Bell />
       </div>
     </div>
