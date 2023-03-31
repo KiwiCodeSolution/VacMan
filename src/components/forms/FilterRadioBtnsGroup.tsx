d8b4ea229ee36946be864c3c773835177fffd447
Alexander Khardin <khporter1984@gmail.com>
2023-03-31 22:51:20 +0300

    create addVacancyForm component

---
+++ src/components/forms/FilterRadioBtnsGroup.tsx
@@ -0,0 +1,32 @@
+import { ReactElement } from "react";
+import { FieldHookConfig } from "formik";
+
+import { IIconProps } from "components/iconsComponents";
+import FilterRadioBtn from "components/forms/FilterRadioBtn";
+
+type FilterRadioBtnsGroupProps = FieldHookConfig<string> & {
+  values: Array<string>;
+  label?: string;
+  LabelIcon?: (props: IIconProps) => JSX.Element;
+};
+
+const FilterRadioBtnsGroup = ({ name, values, label, LabelIcon }: FilterRadioBtnsGroupProps): ReactElement => {
+  return (
+    <div>
+      <div className="flex flex-wrap gap-1 justify-start items-center  text-txt">
+        {LabelIcon && <LabelIcon size="16" className="fill-txt-main" />}
+        {label && <p className="text-base text-txt-main">{label}</p>}
+      </div>
+
+      <ul className="mt-2 flex flex-wrap justify-start gap-3">
+        {values.map((value: string) => (
+          <li key={name + value}>
+            <FilterRadioBtn name={name} value={value} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FilterRadioBtnsGroup;
--- src/components/forms/StarRadioBtnsGroup.tsx
+++ src/components/forms/StarRadioBtnsGroup.tsx
@@ -2,31 +2,41 @@ import { ReactElement, useState } from "react";
 import { FieldHookConfig } from "formik";
 
 import StarRadioBtn from "components/forms/StarRadioBtn";
+import { IIconProps } from "components/iconsComponents";
 
 type StarRadionBtnsGroupProps = FieldHookConfig<string> & {
   values: Array<string>;
+  label?: string;
+  LabelIcon?: (props: IIconProps) => JSX.Element;
 };
 
-const StarRadioBtnsGroup = ({ name, values }: StarRadionBtnsGroupProps): ReactElement => {
+const StarRadioBtnsGroup = ({ name, values, label, LabelIcon }: StarRadionBtnsGroupProps): ReactElement => {
   const [hoveredBtnId, setHoveredBtnId] = useState(0);
   const [selectedBtnId, setSelectedBtnId] = useState(0);
 
   return (
-    <ul className="flex gap-3">
-      {values.map((value: string, idx: number) => (
-        <li key={name + value}>
-          <StarRadioBtn
-            name={name}
-            value={value}
-            btnId={idx + 1}
-            hoveredBtnId={hoveredBtnId}
-            selectedBtnId={selectedBtnId}
-            onBtnHover={(id: number): void => setHoveredBtnId(id)}
-            onBtnSelect={(id: number): void => setSelectedBtnId(id)}
-          />
-        </li>
-      ))}
-    </ul>
+    <div>
+      <div className="flex gap-1 items-center  text-txt">
+        {LabelIcon && <LabelIcon className="fill-txt-main" />}
+        {label && <p className="text-base text-txt-main">{label}</p>}
+      </div>
+
+      <ul className="mt-2 flex justify-center gap-3">
+        {values.map((value: string, idx: number) => (
+          <li key={name + value}>
+            <StarRadioBtn
+              name={name}
+              value={value}
+              btnId={idx + 1}
+              hoveredBtnId={hoveredBtnId}
+              selectedBtnId={selectedBtnId}
+              onBtnHover={(id: number): void => setHoveredBtnId(id)}
+              onBtnSelect={(id: number): void => setSelectedBtnId(id)}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
