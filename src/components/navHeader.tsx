eebb2ca41e7d70be7d6b86f3a38468a52617ee17
Aleksandr-Pod <stdfire@gmail.com>
2023-04-01 18:34:14 +0300

    add Link to to nextBtn

--- src/components/navHeader.tsx
+++ src/components/navHeader.tsx
@@ -9,7 +9,7 @@ interface IProps {
 }
 const NavHeader = ({ prevAddress, text, ref, nextAddress }: IProps) => {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between items-center">
       {prevAddress && (
         <button className="flex-none hover:scale-110 focus:scale-110">
           <Link to={prevAddress}>
@@ -21,10 +21,12 @@ const NavHeader = ({ prevAddress, text, ref, nextAddress }: IProps) => {
       {ref ? <a href={ref}>{text}</a> : <p className="text-center text-2xl">{text}</p>}
 
       {nextAddress && (
-        <button type="button" className="flex gap-px justify-between items-center">
-          <span className="font-semibold text-base text-txt-main mx-3">Skip </span>
-          <Icons.ArrowForward className="fill-txt-main stroke-txt-main" />
-        </button>
+        <Link to={nextAddress}>
+          <button type="button" className="flex gap-px justify-between items-center hover:scale-110 focus:scale-110">
+            <span className="font-semibold text-base text-txt-main mx-3">Skip </span>
+            <Icons.ArrowForward className="fill-txt-main stroke-txt-main" />
+          </button>
+        </Link>
       )}
     </div>
   );
