aedf8229979271a059f8ad9c942c35edf846afde
Aleksandr-Pod <stdfire@gmail.com>
2023-04-01 20:18:39 +0300

    LogOut in Settings

--- src/pages/Private/profilePage.tsx
+++ src/pages/Private/profilePage.tsx
@@ -1,6 +1,6 @@
 import NavHeader from "components/navHeader";
 
 const ProfilePage = () => {
-  return <NavHeader prevAddress="/" text="Reminder" />;
+  return <NavHeader prevAddress="/" text="Profile" />;
 };
 export default ProfilePage;
--- src/pages/Private/settingsPage.tsx
+++ src/pages/Private/settingsPage.tsx
@@ -1,6 +1,20 @@
 import NavHeader from "components/navHeader";
+import Button from "components/ui/button";
+import { useDispatch } from "react-redux";
+import { setIsAuth } from "redux/userSlice";
 
 const SettingsPage = () => {
-  return <NavHeader prevAddress="/" text="Settings" />;
+  const dispatch = useDispatch();
+
+  const logOut = () => {
+    dispatch(setIsAuth(false));
+  };
+
+  return (
+    <>
+      <NavHeader prevAddress="/" text="Settings" />;
+      <Button variant="black" clickFn={logOut}>Log Out</Button>;
+    </>
+  );
 };
 export default SettingsPage;
