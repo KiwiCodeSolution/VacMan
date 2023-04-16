e40d79b4196cdfc6239a7343c9b2d51e3d1701e0
Aleksandr-Pod <stdfire@gmail.com>
2023-04-16 16:37:50 +0300

    bug fixes-2

--- src/pages/Private/archived.tsx
+++ src/pages/Private/archived.tsx
@@ -17,12 +17,12 @@ const Archived = () => {
         </div>
       ) : !archivedVacancies || !archivedVacancies.length ? (
         <>
-          <NavHeader prevAddress="/settings" text="Archive" bg="bg-light" underlined />
+          <NavHeader bg="bg-light" prevAddress="/settings" text="Archive" underlined />
           <div className="mt-20 text-2xl text-txt-main text-center">No archived vacancy</div>
         </>
       ) : (
         <>
-          <NavHeader prevAddress="/settings" text="Archive" underlined />
+          <NavHeader bg="bg-light" prevAddress="/settings" text="Archive" underlined />
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-28 mt-5 items-center gap-4 px-4">
             {archivedVacancies &&
               archivedVacancies.map(vacancy => <ShortNote key={vacancy._id} shortVacancy={vacancy} />)}
