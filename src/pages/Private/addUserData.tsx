60e5d7e3ad0f00f54291abf868db9d52b20e3e96
Aleksandr-Pod <stdfire@gmail.com>
2023-04-26 13:44:55 +0300

    addUserData goBack button

--- src/pages/Private/addUserData.tsx
+++ src/pages/Private/addUserData.tsx
@@ -1,10 +1,18 @@
 import AddUserInfoForm from "components/forms/AddUserInfoForm";
 import NavHeader from "components/navHeader";
+import { useLocation } from "react-router-dom";
 
 const AddUserData = () => {
+  const location = useLocation();
   return (
     <>
-      <NavHeader bg="bg-light" text="User information" skip="/" underlined />
+      <NavHeader
+        prevAddress={location?.state?.from.pathname ?? "/"}
+        bg="bg-light"
+        text="User information"
+        skip="/"
+        underlined
+      />
       <div className="container mx-auto px-4">
         <AddUserInfoForm />
       </div>
