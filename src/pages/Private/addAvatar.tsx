356046bf1eb905bdd6c9a77ef9a01c1089d8bf4c
Aleksandr-Pod <stdfire@gmail.com>
2023-04-26 14:02:43 +0300

    upload avatar btn

--- src/pages/Private/addAvatar.tsx
+++ src/pages/Private/addAvatar.tsx
@@ -1,5 +1,6 @@
 import NavHeader from "components/navHeader";
 import SubHeader from "components/subHeader";
+import Button from "components/ui/button";
 import { useLocation } from "react-router-dom";
 
 /* eslint-disable prettier/prettier */
@@ -9,6 +10,12 @@ const AddAvatar = () => {
     <>
       <NavHeader bg="bg-grey" prevAddress={location?.state?.from.pathname ?? "/"} text="Add avatar" textWhite />
       <SubHeader fill="text-txt-blue" />
+
+      <div className="mx-auto w-40 pt-4 pb-7">
+        <Button variant="black">
+          upload avatar
+        </Button>
+      </div>
     </>
   )
 };
