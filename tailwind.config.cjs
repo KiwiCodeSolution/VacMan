68e87756ff94e214d380031c684af92e5578b5d7
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2023-05-02 17:55:08 +0300

    animation for info

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,15 +6,20 @@ module.exports = {
       keyframes: {
         slide: {
           "0%": {
-            transform: 'translateY(-100%)',
+            transform: "translateY(-100%)",
           },
           "100%": {
-            transform: 'translateY(0)',
-          }
-        }
+            transform: "translateY(0)",
+          },
+        },
+        show: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
       },
       animation: {
-        slide: "slide 1s"
+        slide: "slide 1s",
+        show: "show 3s cubic-bezier(0.4, 0, 0.6, 1)",
       },
       colors: {
         "txt-main": "#5b5b69",
