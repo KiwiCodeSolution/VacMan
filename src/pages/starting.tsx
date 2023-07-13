b2137dac0fed62f0ccb9ff34d1c38f0c3ddbd591
Aleksandr Podmazko <stdfire@gmail.com>
2023-07-13 16:29:24 +0300

    starting page corrected

--- src/pages/starting.tsx
+++ src/pages/starting.tsx
@@ -6,10 +6,13 @@ const StartingPage = () => {
 
   return (
     <div className="flex flex-col h-screen items-center justify-evenly bg-bg-black text-txt-main">
-      <h2 className="text-4xl">Vacancy Manager</h2>
-      <h3 className="text-2xl">by KiWiCode Solution</h3>
+      <h2 className="text-txt-orange text-4xl">Vacancy Manager</h2>
+      <h3 className="text-txt-orange text-2xl">by KiWiCode Solution</h3>
       <Logo active />
-      <h2 className="text-2xl p-10 text-center">{quotes[idx]}</h2>
+      <p className="mx-[-20px]"> Тут могла быть ваша реклама </p>
+      <h2 className="border border-double border-txt-orange h-[40%] w-[80%] rounded-3xl marker:text-3xl p-8 text-center bg-bg-grey text-txt-orange">
+        {quotes[idx]}
+      </h2>
     </div>
   );
 };
