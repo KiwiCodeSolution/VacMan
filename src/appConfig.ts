e905cbf9d7db831fc47d0a0a60d541fdc25b19e8
Aleksandr Podmazko <stdfire@gmail.com>
2023-08-10 21:50:54 +0300

    appConfig serverUrl fixed

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,5 +1,5 @@
-const serverUrl = "https://vacmanserver-production.up.railway.app";
-// const serverUrl = "http://kiwicode.tech:5000";
-// const serverUrl = "http://localhost:3030";
+const serverUrl = "https://vacmanserver-production.up.railway.app/";
+// const serverUrl = "http://kiwicode.tech:5000/";
+// const serverUrl = "http://localhost:3030/";
 
 export default serverUrl;
